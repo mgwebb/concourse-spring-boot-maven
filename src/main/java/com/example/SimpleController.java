157d831ac08ac5f678b0931465d55c01a422649c
mgwebb <mark.webb@cgi.com>
2019-07-03 17:48:19 -0600

    added controller and updated git path

---
+++ src/main/java/com/example/SimpleController.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class SimpleController {
+        @RequestMapping("/")
+        public String getMessage() {
+            return "-- Welcome to SimpleController --";
+        }
+}
