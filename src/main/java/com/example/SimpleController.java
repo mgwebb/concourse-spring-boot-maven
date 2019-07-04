64b3404c5dadf687a41536950d66cade3db957c1
mgwebb <mark.webb@cgi.com>
2019-07-03 18:16:57 -0600

    added missing spring annotation

--- src/main/java/com/example/SimpleController.java
+++ src/main/java/com/example/SimpleController.java
@@ -1,7 +1,9 @@
 package com.example;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class SimpleController {
         @RequestMapping("/")
         public String getMessage() {
