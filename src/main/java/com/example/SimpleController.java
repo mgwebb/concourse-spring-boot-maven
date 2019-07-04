e88b0fa16099a183fae1ee7b1da5a7aaf21a929c
mgwebb <mark.webb@cgi.com>
2019-07-03 18:13:34 -0600

    updated controller

--- src/main/java/com/example/SimpleController.java
+++ src/main/java/com/example/SimpleController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SimpleController {
         @RequestMapping("/")
         public String getMessage() {
-            return "-- Welcome to SimpleController --";
+            return "-- Welcome to SimpleController New and Improved --";
         }
 }
